ddb04e4c16dde01d172c58e1710242970927e99b
Murilo Marega <murilo.marega@ciag.org.br>
2020-10-12 23:21:43 -0300

    priemiro dia nlw

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
+import './styles/global.css';
+
+import Routes from './routes'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>hello NLW</h1>
-      </header>
-    </div>
+    <Routes />
   );
 }
 
